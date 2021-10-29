b311b26e01a7ffd0fa257860fc8da9e8993d7e74
DanyMencu <dany69men@gmail.com>
2021-10-29 14:50:49 +0200

    Code cleaning

--- JS-palindromo/script.js
+++ JS-palindromo/script.js
@@ -3,10 +3,7 @@ const resoult = document.querySelector('.resoult');
 //Immissione di una parola da parte dell'utente
 const userWord = prompt('Inserisci una parola che credi sia palindroma').trim().toLowerCase();
 
-console.log('Parola utente ' + userWord);
-
 const reversWord = reverseChars(userWord);
-console.log('Parola utente al contrario ' + reversWord);
 //Function per riscrivere la parola al contrario
 function reverseChars(word) {
     let reverse = '';
@@ -21,9 +18,7 @@ function reverseChars(word) {
 if (reversWord !== userWord) {
     //Output del risultato se è o non è palindroma la parola inserita
     resoult.innerHTML = (`La parola inserita NON è palindroma, ${userWord} al contrario è ${reversWord}.`);
-    console.log(`La parola inserita NON è palindroma, ${userWord} al contrario è ${reversWord}.`);
 } else {
     //Output del risultato se è o non è palindroma la parola inserita
     resoult.innerHTML = (`La parola inserita è palindroma, ${userWord} al contrario è ${reversWord}.`);
-    console.log(`La parola inserita è palindroma, ${userWord} al contrario è ${reversWord}.`);
 }
